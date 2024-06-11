53dbb1979129c712fe32e0443026e0b3b26d6997
Al-mubarmij <42393721+Al-mubarmij@users.noreply.github.com>
2024-06-11 19:55:25 +0400

    connecting to mysql

--- module1/LittleLemon/settings.py
+++ module1/LittleLemon/settings.py
@@ -79,9 +79,20 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mydb': {   
+ 
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'littlelemon',   
+        'USER': 'admindjango',   
+        'PASSWORD': 'employee@123',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }     
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -123,3 +134,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+DATABASES = {
+    'default': { 
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': BASE_DIR / 'db.sqlite3', 
+    },
+    'mydb': {   
+ 
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'my_database',   
+        'USER': 'root',   
+        'PASSWORD': 'your_password',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        } 
+
+'''
