d2c6ac21b1cf5ff5a25b15a48debd2db2ca3095d
Al-mubarmij <42393721+Al-mubarmij@users.noreply.github.com>
2024-06-11 23:00:38 +0400

    CRUD on Booking using ModelViewSet

--- module1/LittleLemon/urls.py
+++ module1/LittleLemon/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant.views import UserViewSet, BookingViewSet
 
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'tables', BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('restaurant/booking/', include(router.urls)),
 ]
+'''
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    
+# The api-auth route is defined to let you use the browsable API feature of DRF.  
+
+
+'''
--- module1/restaurant/views.py
+++ module1/restaurant/views.py
@@ -1,5 +1,63 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Menu, Booking 
+from django.contrib.auth.models import User
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+'''
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data) # Returns JSON 
+'''
+
+
+class MenuView(APIView):
+
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+class UserViewSet(viewsets.ViewSet):
+    queryset = User.objects.all()
+    serializer = UserSerializer(queryset,many=True)
+    permission_classes = [IsAuthenticated]
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+'''
+# Declaring viewSet class example:
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
+
+'''
