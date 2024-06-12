8076f68c9638b5d6287c709f5763f4992d1f2bb3
Al-mubarmij <42393721+Al-mubarmij@users.noreply.github.com>
2024-06-12 13:15:33 +0400

    Delete module1 directory

--- module1/LittlelemonAPI/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import MenuItem
-from .serializers import MenuItemSerializer
-from rest_framework.generics import ListCreateAPIView
-# Create your views here.
-
-class MenuItemvView(ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
--- module1/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from LittlelemonAPI.models import MenuItem
-
-class MenuTest(TestCase):
-    def test_get_item(self):
-        item = MenuItem.objects.create(title='IceCream', price=80, inventory=100)
-        self.assertEqual(item.get_item(), 'IceCream : 80')
